903e4fe8fa7a67baadd881bee211ae3c27b0f29f
Cedric Mansey <cmansey@c1r9s6.42lausanne.ch>
2022-11-24 16:09:05 +0100

    debut

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,24 @@
 /*   By: cmansey <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:57:47 by cmansey           #+#    #+#             */
-/*   Updated: 2022/11/21 15:57:48 by cmansey          ###   ########.fr       */
+/*   Updated: 2022/11/24 16:05:49 by cmansey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
